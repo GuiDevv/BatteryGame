001960b6c6fedc8258aa09bb00b037ffb6c1b5a3
Guilherme Gonçalves <guilherme_1300@hotmail.com>
2022-06-05 14:04:58 -0300

    Primeiro Menu

---
+++ PuzzleGame/src/app/battery/battery.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-battery',
+  templateUrl: './battery.component.html',
+  styleUrls: ['./battery.component.scss'],
+})
+export class BatteryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
